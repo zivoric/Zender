3c5b36dccf03755c79ec5c3235cf4cdc28c6ba75
BuildTools <unconfigured@null.spigotmc.org>
2022-05-07 20:01:08 -0400

    DOM Processor

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import './scss/main.scss';
 
 import {ZenderAPI} from "./lib/api";
 import {IZenderAPI} from "./lib/types/api";
-export {LaTeXProcessor} from './lib/processor';
+export {DOMProcessor, LaTeXProcessor} from './lib/processor';
 
 let API: IZenderAPI;
 
--- src/lib/types/math.ts
+++ src/lib/types/math.ts
@@ -1,18 +1,30 @@
-export type MathUnit = string | JsMathObject;
-export type MathGroup = MathUnit | MathUnit[];
+export type MU = string | JsMathObject;
+export type MG = MU | MU[];
 
 export interface JsMathObject {
     operator: string,
-    optional?: MathGroup[]
-    required?: MathGroup[]
+    optional?: MG[]
+    required?: MG[]
 }
-/*
-let o : JsMathObject = {
-    operator: "frac",
-    arguments: [4,[4,'x']]
-};
-let o = {
-    operator: "frac",
-    arguments: [4,[4,'x']]
-};
+
+export const SelectionStart: unique symbol = Symbol();
+export const SelectionEnd: unique symbol = Symbol();
+export type MathUnit = string | JsMathObj | typeof SelectionStart | typeof SelectionEnd;
+export type MathGroup = (MathGroup|MathUnit)[];
+export type ArgumentList = {[arg: string]: MathGroup};
+export interface JsMathObj {
+    operator: string,
+    arguments: ArgumentList
+}
+
+/* example object:
+
+    [
+        "345","x","*",
+        {
+            operator: "frac",
+            arguments: {"numer":["3"],"denom":["40","y"]}
+        }
+    ]
+
 */
